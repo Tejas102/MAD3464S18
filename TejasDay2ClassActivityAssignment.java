b10b4e3ce861c1f91aaff956aa5be4870842bcbb
Tejas102 <39779022+Tejas102@users.noreply.github.com>
2018-05-31 17:22:13 -0400

    Add files via upload

---
+++ TejasDay2ClassActivityAssignment.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tejas
+ */
+public class TejasDay2ClassActivityAssignment {
+    public static void main(String args[])
+    {
+        System.out.println("For 5 Lines");
+        for (int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=5; j++)
+        {
+         if(i==1 || j ==1 || i==5 || j==5)   
+         {
+             System.out.print("*");
+         }
+            else
+            {
+                    System.out.print(" ");
+                    }
+    }
+        System.out.println();
+        }
+    }
+    }
+
+
+    
